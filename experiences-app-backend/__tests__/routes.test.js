5c5b37f49306e44a5f7daa70dd4483d23c22d105
meganluisa <megan.white@mapbox.com>
2022-05-26 14:18:48 -0600

    finished get api/images test

--- experiences-app-backend/__tests__/routes.test.js
+++ experiences-app-backend/__tests__/routes.test.js
@@ -3,15 +3,27 @@ const mongoose = require("mongoose");
 const router = require("../routes/routes.js");
 const request = require("supertest");
 const express = require("express");
+const Photo = require('../models/photo');
+const {getImages} = request('../routes/routes')
 
 const app = new express();
 app.use('/', router);
 
 describe("Get all Images", () => {
+
+  beforeAll(() => {
+    Photo.find = jest.fn().mockResolvedValue([{
+      "_id": "62685303890952862c5712fd",
+		  "id": "8b5e4646-397b-4f2d-9df1-4d9391a8dcf8",
+		  "name": "IMG_3837.jpg",
+		  "coordinates": [
+			  -77.0264,
+			  38.8852
+		  ]
+    }])
+  })
   test("Get array of image data", async () => {
-    const res = await request(app).get('api/images');
-    console.log(res);
-    // const images = await getImages();
-    expect(res).toEqual(expect.arrayContaining(res)); // ensures that an array is returned from the getImages function, intended to be the endpoint that returns an array of all images
+    const res = await request(app).get('/api/images');
+    expect(res.body).toEqual(expect.arrayContaining(res.body)); // ensures that an array is returned from the getImages function, intended to be the endpoint that returns an array of all images
   })
 });
